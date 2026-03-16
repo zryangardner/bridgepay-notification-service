045a992cff275fc49bf33c8fec309d35ffa4b365
zryangardner <zryangardner@gmail.com>
2026-03-16 16:44:11 -0600

    chore: initialize bridgepay-notification-service with Spring Boot/Kotlin scaffolding
    - Spring Boot 3.5.11, Kotlin, Java 21, Maven
    - Add Spring Cloud AWS BOM (3.3.0) and spring-cloud-aws-starter-sqs dependency

---
+++ src/main/kotlin/com/bridgepay/notificationservice/BridgepayNotificationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.bridgepay.notificationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BridgepayNotificationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<BridgepayNotificationServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/bridgepay/notificationservice/BridgepayNotificationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bridgepay.notificationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BridgepayNotificationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
